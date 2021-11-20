66ad718076c8a09a2623662b60b631ac00c61ded
Ryazanskiy.M <ryazanskiy.m@wildberries.ru>
2021-11-20 17:09:37 +0300

    Longest Subarray with Ones after Replacement

---
+++ sliding window/8 - Longest Subarray with Ones after Replacement.py
@@ -0,0 +1,43 @@
+"""
+Given an array containing 0s and 1s, if you are allowed to replace no more than ‘k’ 0s with 1s,
+find the length of the longest contiguous subarray having all 1s.
+
+Input: Array=[0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], k=2
+Output: 6
+Explanation: Replace the '0' at index 5 and 8 to have the longest
+contiguous subarray of 1s having length 6.
+
+Input: Array=[0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1], k=3
+Output: 9
+Explanation: Replace the '0' at index 6, 9, and 10 to have
+the longest contiguous subarray of 1s having length 9.
+"""
+
+
+def length_of_longest_substring(arr, k):
+    window_start, max_length, max_repeat_letter_count = 0, 0, 0
+    frequency_map = {}
+    for window_end in range(len(arr)):
+        right_char = arr[window_end]
+        if right_char not in frequency_map:
+            frequency_map[right_char] = 0
+        frequency_map[right_char] += 1
+        max_repeat_letter_count = max(
+            max_repeat_letter_count,
+            frequency_map[right_char]
+        )
+        if (window_end - window_start + 1 - max_repeat_letter_count) > k:
+            left_char = arr[window_start]
+            frequency_map[left_char] -= 1
+            window_start += 1
+
+        max_length = max(max_length, window_end - window_start + 1)
+    return max_length
+
+
+def main():
+    print(length_of_longest_substring([0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], 2))
+    print(length_of_longest_substring([0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1], 3))
+
+
+main()
