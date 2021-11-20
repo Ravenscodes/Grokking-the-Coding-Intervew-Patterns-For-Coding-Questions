ca9c1f1684371b56c9d1667e8be5c3185feda755
Ryazanskiy.M <ryazanskiy.m@wildberries.ru>
2021-11-20 17:14:37 +0300

    Longest Subarray with Ones after Replacement

--- sliding window/8 - Longest Subarray with Ones after Replacement.py
+++ sliding window/8 - Longest Subarray with Ones after Replacement.py
@@ -15,22 +15,15 @@ the longest contiguous subarray of 1s having length 9.
 
 
 def length_of_longest_substring(arr, k):
-    window_start, max_length, max_repeat_letter_count = 0, 0, 0
-    frequency_map = {}
+    window_start, max_length, max_ones_count = 0, 0, 0
     for window_end in range(len(arr)):
-        right_char = arr[window_end]
-        if right_char not in frequency_map:
-            frequency_map[right_char] = 0
-        frequency_map[right_char] += 1
-        max_repeat_letter_count = max(
-            max_repeat_letter_count,
-            frequency_map[right_char]
-        )
-        if (window_end - window_start + 1 - max_repeat_letter_count) > k:
-            left_char = arr[window_start]
-            frequency_map[left_char] -= 1
-            window_start += 1
+        if arr[window_end] == 1:
+            max_ones_count += 1
 
+        if (window_end - window_start + 1 - max_ones_count) > k:
+            if arr[window_start] == 1:
+                max_ones_count -= 1
+            window_start += 1
         max_length = max(max_length, window_end - window_start + 1)
     return max_length
 
